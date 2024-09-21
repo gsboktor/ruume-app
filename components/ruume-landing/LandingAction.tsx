05f30f06cdf23213f15e06ad292623107c7f93cd
George Boktor <gsboktor@gmail.com>
2024-09-21 10:04:52 -0500

    cleanup

--- components/ruume-landing/LandingAction.tsx
+++ components/ruume-landing/LandingAction.tsx
@@ -1,19 +1,37 @@
 import React, { useCallback, useState } from 'react';
-import { Pressable, Text } from 'react-native';
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { Pressable } from 'react-native';
+import Animated, {
+  FadeOut,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
 import * as Haptics from 'expo-haptics';
 import styled from 'styled-components/native';
 
-const LandingActionButton = styled(Animated.View)<{ pressed: boolean }>`
+const LandingActionButton = styled(Animated.View)`
+  position: relative;
   width: 275px;
   height: 90px;
   background-color: #fff;
   justify-content: center;
+  border-radius: 50%;
   align-items: center;
   flex-wrap: wrap;
   text-align: center;
   box-shadow: 0px 8px 10px rgba(93, 93, 93, 0.5);
+`;
+
+const LandingActionText = styled(Animated.Text)`
+  font-size: 20px;
+  font-weight: 300;
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
 `;
 
 export const LandingAction = ({
@@ -22,44 +40,41 @@ export const LandingAction = ({
   setLandingContentVisible: (visible: boolean) => void;
 }) => {
   const width = useSharedValue(275);
-  const opacity = useSharedValue(1);
-  const borderRadius = useSharedValue(50);
+  const height = useSharedValue(90);
 
   const [contentVisible, setContentVisible] = useState(true);
 
   const animatedStyle = useAnimatedStyle(() => ({
     width: width.value,
-    opacity: opacity.value,
-    borderRadius: borderRadius.value,
+    height: height.value,
   }));
 
   const handlePress = useCallback(() => {
     setContentVisible(false);
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
-    width.value = withTiming(0, { duration: 300 }, () => {
-      runOnJS(setLandingContentVisible)(true);
-    });
-    borderRadius.value = withTiming(0, { duration: 250 });
-  }, [width, borderRadius, setLandingContentVisible]);
+
+    width.value = withSequence(
+      withTiming(290, { duration: 75 }),
+      withTiming(12, { duration: 300 }, (finished) => {
+        if (finished) {
+          height.value = withDelay(
+            25,
+            withTiming(0, { duration: 200 }, (finished) => {
+              if (finished) {
+                runOnJS(setLandingContentVisible)(true);
+              }
+            }),
+          );
+        }
+      }),
+    );
+  }, [width, height, setLandingContentVisible]);
 
   return (
     <Pressable onPress={handlePress}>
-      {({ pressed }) => (
-        <LandingActionButton pressed={pressed} style={animatedStyle}>
-          {contentVisible && (
-            <Text
-              style={{
-                fontSize: 20,
-                fontWeight: '300',
-                width: '100%',
-                textAlign: 'center',
-              }}
-            >
-              Let&apos;s Ruume
-            </Text>
-          )}
-        </LandingActionButton>
-      )}
+      <LandingActionButton style={animatedStyle}>
+        {contentVisible && <LandingActionText exiting={FadeOut.duration(100)}>Let&apos;s Ruume</LandingActionText>}
+      </LandingActionButton>
     </Pressable>
   );
 };
