18429d57d2f4f047ac9ca0967b8edac696d4266b
George Boktor <gsboktor@gmail.com>
2024-09-22 14:10:07 -0500

    feat: animation and bg tweaks; still boilerplate

--- app/(tabs)/ruume-home.tsx
+++ app/(tabs)/ruume-home.tsx
@@ -3,10 +3,19 @@ import { View } from 'react-native';
 
 import { BaseText } from '@Ruume/components/shared';
 
+import styled from 'styled-components/native';
+
+const StyledHomeContainer = styled(View)`
+  height: 100%;
+  background-color: black;
+`;
+
 export default function RuumeHome() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <BaseText type="bold">Welcome to Ruume Home!</BaseText>
-    </View>
+    <StyledHomeContainer>
+      <View style={{ flex: 1, alignItems: 'center', marginTop: 100 }}>
+        <BaseText type="bold">Welcome to Ruume Home!</BaseText>
+      </View>
+    </StyledHomeContainer>
   );
 }
--- components/tab-bar/TabBar.tsx
+++ components/tab-bar/TabBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { Dimensions, Pressable } from 'react-native';
-import Animated, { Easing, useSharedValue, withDelay, withTiming } from 'react-native-reanimated';
+import { Dimensions, Pressable, View } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import styled from 'styled-components/native';
@@ -8,16 +8,27 @@ import styled from 'styled-components/native';
 const screenWidth = Dimensions.get('window').width;
 const tabBarWidth = 175;
 
-const StyledTabBarContainer = styled(Animated.View)`
+const BarContainer = styled(View)`
+  position: relative;
   background-color: #000;
+  width: 100%;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledTabBarContainer = styled(Animated.View)`
+  background-color: #fff;
   display: flex;
   flex-direction: row;
   width: 175px;
   height: 80px;
   border-radius: 100px;
   position: absolute;
-  bottom: 20px;
+  bottom: 40px;
   left: ${(screenWidth - tabBarWidth) / 2}px;
+  align-self: center;
   justify-content: space-between;
 `;
 
@@ -32,21 +43,23 @@ export type TabBarProps = BottomTabBarProps;
 export default function TabBar({ state, descriptors, navigation }: TabBarProps) {
   const yPosition = useSharedValue(100);
   useEffect(() => {
-    yPosition.value = withDelay(25, withTiming(0, { duration: 500, easing: Easing.elastic(1.2) }));
+    yPosition.value = withSpring(0, { stiffness: 200, damping: 15, mass: 1 });
   }, [yPosition]);
 
   return (
-    <StyledTabBarContainer style={{ transform: [{ translateY: yPosition }] }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const isFocused = state.index === index;
+    <BarContainer>
+      <StyledTabBarContainer style={{ transform: [{ translateY: yPosition }] }}>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const isFocused = state.index === index;
 
-        return (
-          <StyledTabBarItem key={route.key} onPress={() => navigation.navigate(route.name)}>
-            {options.tabBarIcon && options.tabBarIcon({ focused: isFocused, color: '#fff', size: 24 })}
-          </StyledTabBarItem>
-        );
-      })}
-    </StyledTabBarContainer>
+          return (
+            <StyledTabBarItem key={route.key} onPress={() => navigation.navigate(route.name)}>
+              {options.tabBarIcon && options.tabBarIcon({ focused: isFocused, color: '#000', size: 24 })}
+            </StyledTabBarItem>
+          );
+        })}
+      </StyledTabBarContainer>
+    </BarContainer>
   );
 }
